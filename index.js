45cbd36872f5c082d0070e52ff54eb60198f8fa9
JoshuaF <joshuarpf@gmail.com>
2018-11-27 15:30:26 +0800

    added working upload with checker if there is a file present

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const formidable 	= require('formidable')
 const http 			= require('http')
 const util 			= require('util')
 const fs 			= require('fs-extra')
+const os  			= require('os')
 
 const app 			= express()
 const port 			= 3000
 
+const helper 		= require('./helpers/helper.js')
 
 app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-	res.render('index.hbs', { title: "jambawamba", foo:"foobar"})
+	res.render('index.hbs', { title: "jambawamba", foo:"foobar" })
 })
 
 app.get('/about', (req, res) => {
@@ -25,18 +27,49 @@ app.get('/about', (req, res) => {
 app.post('/', (req, res) => {
 
 	if (req.url == '/' && req.method.toLowerCase() == 'post') {
-	    // parse a file upload
-	    var form = new formidable.IncomingForm();
 
-	    form.parse(req, function(err, fields, files) {
-	      res.writeHead(200, {'content-type': 'text/plain'});
-	      res.write('received upload:\n\n');
-	      res.end(util.inspect({fields: fields, files: files}));
-	      // console.log(fields)
-	    });
+		var form = new formidable.IncomingForm();
 
-	    return;
-	  }
+		form.uploadDir = __dirname + '/uploads/'
+
+		form.parse(req)
+
+		/**
+		 * Sample checking on parsed form data
+		 */
+		// form.parse(req, (error, fields, files) => {
+		// 	if(error == null && files.sampleFile.size > 0)
+		// 	{
+		// 	}
+		// })
+
+
+		// form.on('fileBegin', (name, file) => {
+		// 	if(file) {
+		// 		file.path = __dirname + '/uploads/' + file.name;	
+		// 	}
+			
+		// })
+
+		form.on('file', (name, file) => {
+			if(file.size > 0)
+			{
+				// form.on('fileBegin', (name, file) => {
+				// 	if(file) {
+				// 		file.path = __dirname + '/uploads/' + file.name;	
+				// 	}
+				// })
+				// console.log("there is a file --> " + os.tmpdir())
+				fs.rename(file.path, __dirname + '/uploads/' + file.name)
+				res.send("done")
+			} else {
+				res.render('index.hbs', { title: "jambawamba", foo:"foobar", 'errorMessage':"No files uploaded..." })
+			}
+
+
+		})
+
+	}
 
 })
 
