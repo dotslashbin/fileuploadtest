23c7e832ae0a8029da46fb9a241a792520bd3593
JoshuaF <joshuarpf@gmail.com>
2018-12-06 22:43:01 +0800

    Added example of object deconstruction and generating object id

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,18 @@
-const MongoClient = require('mongodb').MongoClient
+/**
+ * Basic Example
+ */
+// const MongoClient = require('mongodb').MongoClient
+
+/**
+ * Object deconstruction example
+ */
+const { MongoClient, ObjectID } = require('mongodb')
+
+/**
+ * Create mongodb object id like mongodb
+ */
+var obj = new ObjectID(); 
+console.log("GEERATED OBJECT ID :" + obj) // Regular object id
 
 MongoClient.connect('mongodb://nodeapp:pass1234@localhost:27017/appdb', { useNewUrlParser: true }, (err, client) => {
 	if(err) {
@@ -10,10 +24,10 @@ MongoClient.connect('mongodb://nodeapp:pass1234@localhost:27017/appdb', { useNew
 
 	var db = client.db('appdb')
 
-	db.collection('Todos').insertOne({ text: "aefenother", completed: false}, (err, result) =>  {
+	db.collection('Todos').insertOne({ text: "deconstruct", completed: false}, (err, result) =>  {
 		if(err) {
 			return console.log(' Unable to add the data', err)
-		}
+		} 
 	})
 
 	client.close(); 
